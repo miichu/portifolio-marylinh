83500e30a7faac720de98d2ad23f087fad33229d
My Thao Nguyen <mythng94@gmail.com>
2023-08-22 13:33:46 +0200

    🧼 Clean up unecessary code & add router deps

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,12 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import Header from "./components/nav/Header";
 import Biography from "./pages/biography/Biography";
 import Home from "./pages/home/Home";
 
 const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+      <Header />
       <h1>Mary Linh sin portifolio</h1>
       <div className="card">
         <p>
---
+++ src/components/nav/Header.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Header = () => {
+  return <>this is a header</>;
+};
+
+export default Header;
