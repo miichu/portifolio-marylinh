3ba78fdadd37d71c1afb67c75943deb6c0fab9ce
My Thao Nguyen <mythng94@gmail.com>
2023-08-26 22:02:06 +0200

    🚀 Prepare build

--- src/pages/projects/Projects.tsx
+++ src/pages/projects/Projects.tsx
@@ -1,10 +1,10 @@
-interface IProject {
-  name: string;
-  id: string;
-  // img: string;
-}
+// interface IProject {
+//   name: string;
+//   id: string;
+//   // img: string;
+// }
 
-const projects: IProject[] = [{ name: "project1", id: "project1" }];
+// const projects: IProject[] = [{ name: "project1", id: "project1" }];
 
 const Projects = () => {
   return <>This is supposed to be a view page for projects</>;
