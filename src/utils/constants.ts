805e5d6b48d9a08d2dff88120f0147ede9bc1022
My Thao Nguyen <mythng94@gmail.com>
2023-08-27 19:38:58 +0200

    ✨ Added content for AboutMe and photos
    - Added breakpoints and device breakpoints for later
    - Fixed centering of content
    - Added photos
    - Added content About Me and made it responsive

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -7,3 +7,22 @@ export const SPACING_S = "16px";
 export const SPACING_M = "32px";
 export const SPACING_L = "40px";
 export const SPACING_XL = "48px";
+
+const breakpoints = {
+  xs: "320px",
+  sm: "640px",
+  md: "768px",
+  lg: "1024px",
+  xl: "1280px",
+  "2xl": "1536px",
+};
+
+// NB! can't seem to be able to use these directly for now. ts-styled-components(999) error
+export const devices = {
+  xs: `(min-width: ${breakpoints.xs})`,
+  sm: `(min-width: ${breakpoints.sm})`,
+  md: `(min-width: ${breakpoints.md})`,
+  lg: `(min-width: ${breakpoints.lg})`,
+  xl: `(min-width: ${breakpoints.xl})`,
+  "2xl": `(min-width: ${breakpoints["2xl"]})`,
+};
